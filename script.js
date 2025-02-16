08f7bb045f1f042834d74c6e776372bc59ef8b99
nugmara <dagmarag0@gmail.com>
2025-02-16 14:44:26 +0100

    Create class projectiles

--- script.js
+++ script.js
@@ -25,10 +25,74 @@ class Player {
     }
 }
 
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+
+    draw() {
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI *2, false)
+        ctx.fillStyle = this.color
+        ctx.fill()
+    }
+
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
+
 const x = canvas.width / 2
 const y = canvas.height / 2
 
 const player = new Player(x, y, 30, 'pink');
-player.draw()
 
-console.log(player)
+const projectile = new Projectile(
+    canvas.width / 2, 
+    canvas.height / 2, 
+    5, 
+    'purple',
+    {
+        x: 1,
+        y: 1
+    }
+)
+
+const projectiles = []
+
+function animate() {
+    requestAnimationFrame(animate)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    player.draw()
+    projectiles.forEach(projectile => {
+        projectile.update()
+    })
+}
+
+addEventListener('click', (e) => {
+
+    const angle = Math.atan2(
+        e.clientY - canvas.height / 2,
+        e.clientX - canvas.width / 2
+    )
+    const velocity = {
+        x: Math.cos(angle),
+        y: Math.sin(angle)
+    }
+    projectiles.push(
+        new Projectile(
+        canvas.width / 2, 
+        canvas.height / 2, 
+        5, 
+        'purple', 
+        velocity
+    ))
+})
+
+animate()
